426ec2a234c607edb5e8045c97c866febaa4f09b
smcmanus <rscottmcmanusjr@gmail.com>
2016-12-30 22:15:04 -0500

    Added tests

--- SolarDoomsday/main.py
+++ SolarDoomsday/main.py
@@ -4,7 +4,11 @@ def answer(area):
     squares = []
     while (area > 0):
         largestSquareEdge = int(math.floor(math.sqrt(area)))
-        squares.append(largestSquareEdge)
-        area = area - largestSquareEdge * largestSquareEdge
+        largestSquare = largestSquareEdge * largestSquareEdge
+        squares.append(largestSquare)
+        area = area - largestSquare
     
     return squares
+
+list1 = answer(101)
+list2 = answer(12)
